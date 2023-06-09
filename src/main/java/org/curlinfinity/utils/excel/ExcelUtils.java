ccf52a424ffba58ee2f6638dc2d8ff6984529231
Abhishek Thakur <abhithakur.id@gmail.com>
2023-06-09 23:09:33 +0530

    Added method for column filtering

--- src/main/java/org/curlinfinity/utils/excel/ExcelUtils.java
+++ src/main/java/org/curlinfinity/utils/excel/ExcelUtils.java
@@ -4,11 +4,12 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ExcelUtils {
-
     public Map<String, Integer> getColumnNameIndex(Sheet sheet) {
         return getColumnNameIndex(sheet, 0);
     }
@@ -26,6 +27,27 @@ public class ExcelUtils {
             }
 
             count++;
+        }
+
+        return result;
+    }
+
+    public Map<String, List<Cell>> filterColumns(Sheet sheet, String[] columnNames) {
+        Map<String, List<Cell>> result = new HashMap<>();
+        for (String columnName: columnNames) {
+            result.put(columnName, new ArrayList<>());
+        }
+
+        Map<String, Integer> columnNameIndex = getColumnNameIndex(sheet);
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            Row row = sheet.getRow(i);
+
+            for (Map.Entry<String, Integer> entry: columnNameIndex.entrySet()) {
+                String columnName = entry.getKey();
+                Integer columnIndex = entry.getValue();
+
+                result.get(columnName).add(row.getCell(columnIndex));
+            }
         }
 
         return result;
